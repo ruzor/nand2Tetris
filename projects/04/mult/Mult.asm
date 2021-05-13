e5073c2ac214b467f78a180c3ff3fc84880e99e7
ruzor <jwokomaty@gmail.com>
2021-05-13 01:51:46 +0100

    Implement multiplication function w/Hack

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,32 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    @2
+    M=0
+
+    @0
+    D=M
+    @END
+    D;JEQ // if (RAM[0]==0) goto END
+
+    @1
+    D=M
+    @END
+    D;JEQ // if (RAM[1]==0) goto END
+
+(LOOP)
+    @0
+    M=M-1 // RAM[0]--
+    D=M
+    @END
+    D;JLT // if (RAM[0]<0) goto END
+
+    @1
+    D=M
+    @2
+    M=D+M // RAM[2]=RAM[1]+RAM[2]
+    @LOOP
+    0;JMP // goto LOOP
+(END)
+    @END
+    0;JMP
